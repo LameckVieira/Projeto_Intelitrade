a4c806d01efbb04007975a865358b54f20454b3b
Yan Oliveira Ferreira <44156568890@info.local>
2021-11-10 09:22:09 -0300

    login e cadastro

--- Front_Web/intelitrader-app/src/index.js
+++ Front_Web/intelitrader-app/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+//rotas
+import Login from './pages/Login'
+import Cadastro from './pages/Cadastro'
+
+const routing = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" componet={Login} element={<Login />}/>
+        <Route path="/" componet={Cadastro} element={<Cadastro />}/>
+      </Routes>
+    </Router>
+  </React.StrictMode >
+)
+
+ReactDOM.render(
+  routing,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
